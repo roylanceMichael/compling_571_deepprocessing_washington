7d89388bb60cf2c357501df4d49537ae56566c74
roylanceMichael <roylance.michael@gmail.com>
2014-01-09 19:06:09 -0700

    added a basic test

---
+++ test/grammarTests.py
@@ -0,0 +1,29 @@
+import unittest
+import nltk
+
+class GrammarTests(unittest.TestCase):
+	def test_basicGrammarExample(self):
+		groucho_grammar = nltk.parse_cfg("""
+S -> NP VP
+PP -> P NP
+NP -> Det N | Det N PP | 'I'
+VP -> V NP | VP PP
+Det -> 'an' | 'my'
+N -> 'elephant' | 'pajamas'
+V -> 'shot'
+P -> 'in'
+""")
+
+		sent = ['I', 'shot', 'an', 'elephant', 'in', 'my', 'pajamas']
+
+		parser = nltk.ChartParser(groucho_grammar)
+
+		trees = parser.nbest_parse(sent)
+
+		self.assertTrue(len(trees) == 2)
+
+def main():
+    unittest.main()
+
+if __name__ == '__main__':
+        main()
