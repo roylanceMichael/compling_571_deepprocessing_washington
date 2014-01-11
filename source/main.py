fe3020e2f62f10b635951f93ef3f88508c63aee1
Mike Roylance <roylance.michael@gmail.com>
2014-01-11 16:25:42 -0700

    latest changes

--- source/main.py
+++ source/main.py
@@ -1,7 +1,35 @@
 import nltk
+import sys
+import re
 
 def main():
-	print 'test'
+	# build grammar file url
+	grammarFile = "file:" + sys.argv[1]
+	sentenceFile = sys.argv[2]
+
+	# read in grammar
+	grammar = nltk.data.load(grammarFile)
+	
+	# build chart parser
+	parser = nltk.parse.EarleyChartParser(grammar)
+
+	# read in sentences
+	input_stream = open(sentenceFile)
+
+	line = input_stream.readline()
+
+	while(line):
+		words = nltk.word_tokenize(line.strip().lower())
+		
+		print words
+		# print parser.nbest_parse(words)
+
+		line = input_stream.readline()
+
+	# for each sentence
+	# 	delimit by space, print out bracked sentence
+	#	print out number of parses
+	# end, print out average
 
 if __name__ == '__main__':
         main()
--- test/grammarTests.py
+++ test/grammarTests.py
@@ -22,6 +22,99 @@ P -> 'in'
 
 		self.assertTrue(len(trees) == 2)
 
+	def test_loadToyGrammar(self):
+		grammar = nltk.data.load("file:../testDocs/toy.cfg")
+
+		parser = nltk.parse.EarleyChartParser(grammar)
+
+		sent = ['the', 'dog', 'sat']
+
+		trees = parser.nbest_parse(sent)
+
+		self.assertTrue(grammar != None)
+
+	def test_firstSentence(self):
+		grammar = nltk.data.load("file:../testDocs/grammar.cfg")
+
+		parser = nltk.parse.EarleyChartParser(grammar)
+
+		sent = ['scientists', 'rescued', 'a', 'mouse', 'immune', 'system']
+
+		trees = parser.nbest_parse(sent)
+
+		self.assertTrue(len(trees) > 0)
+
+	def test_secondSentence(self):
+		grammar = nltk.data.load("file:../testDocs/grammar.cfg")
+
+		parser = nltk.parse.EarleyChartParser(grammar)
+
+		sent = ['will', 'this', 'work', 'in', 'humans', '?']
+
+		trees = parser.nbest_parse(sent)
+
+		self.assertTrue(len(trees) > 0)
+
+	def test_thirdSentence(self):
+		grammar = nltk.data.load("file:../testDocs/grammar.cfg")
+
+		parser = nltk.parse.EarleyChartParser(grammar)
+
+		# They published their research today online.
+		sent = nltk.word_tokenize('they published their research today online.')
+
+		trees = parser.nbest_parse(sent)
+
+		self.assertTrue(len(trees) > 0)
+
+	def test_fourthSentence(self):
+		grammar = nltk.data.load("file:../testDocs/grammar.cfg")
+
+		parser = nltk.parse.EarleyChartParser(grammar)
+
+		# They published their research today online.
+		sent = nltk.word_tokenize('the immune response is alerted by dendritic cells.')
+
+		trees = parser.nbest_parse(sent)
+
+		self.assertTrue(len(trees) > 0)
+
+	def test_fifthSentence(self):
+		grammar = nltk.data.load("file:../testDocs/grammar.cfg")
+
+		parser = nltk.parse.EarleyChartParser(grammar)
+
+		# They published their research today online.
+		sent = nltk.word_tokenize('they capture infected cells and display fragments of the pathogen.')
+
+		trees = parser.nbest_parse(sent)
+
+		self.assertTrue(len(trees) > 0)
+
+	def test_sixthSentence(self):
+		grammar = nltk.data.load("file:../testDocs/grammar.cfg")
+
+		parser = nltk.parse.EarleyChartParser(grammar)
+
+		# They published their research today online.
+		sent = nltk.word_tokenize('dr Jose Villadangos is a researcher of the immune system.')
+
+		trees = parser.nbest_parse(sent)
+
+		self.assertTrue(len(trees) > 0)
+
+	def test_seventhSentence(self):
+		grammar = nltk.data.load("file:../testDocs/grammar.cfg")
+
+		parser = nltk.parse.EarleyChartParser(grammar)
+
+		# They published their research today online.
+		sent = nltk.word_tokenize('dr Jose Villadangos is a researcher of the immune system.')
+
+		trees = parser.nbest_parse(sent)
+
+		self.assertTrue(len(trees) > 0)
+
 def main():
     unittest.main()
 
