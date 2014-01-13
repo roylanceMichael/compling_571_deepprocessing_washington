544e83be06baa5b5808643c00ee79a7b46cb65de
Mike Roylance <roylance.michael@gmail.com>
2014-01-12 19:53:01 -0700

    this should fix the condor command submit

--- hw1.cmd
+++ hw1.cmd
@@ -7,8 +7,8 @@
  
 Universe   = vanilla
  
-Executable  = hw1.sh
-Arguments   = testDocs/grammar.cfg testDocs/sentences.txt
+Executable  = bash
+Arguments   = "hw1.sh testDocs/grammar.cfg testDocs/sentences.txt"
 Log         = hw1.log
 Output      = hw1.out
 Error       = hw1.err
