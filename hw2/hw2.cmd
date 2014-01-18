2b65c478afeb975f393ee7899c9403fa15b04e50
Mike Roylance <roylance.michael@gmail.com>
2014-01-17 20:24:30 -0700

    added hw2 template

---
+++ hw2/hw2.cmd
@@ -0,0 +1,16 @@
+####################
+#
+# Mike Roylance UW NetID roylance
+# LING 571 Homework 2
+#
+####################
+ 
+Universe   = vanilla
+ 
+Executable  = hw2.sh
+Arguments   = "testDocs/grammar.cfg testDocs/sentences.txt"
+Log         = hw2.log
+Output      = hw2.out
+Error       = hw2.err
+Notification=Error
+Queue
---
+++ hw2/source/main.py
@@ -0,0 +1,10 @@
+# Mike Roylance - roylance@uw.edu
+import nltk
+import sys
+import re
+
+def main():
+	pass
+
+if __name__ == '__main__':
+        main()
