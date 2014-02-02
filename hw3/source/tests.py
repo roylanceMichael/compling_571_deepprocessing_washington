dcf9d1fcd3d567b32a34b24c2615d35ad06dd1bc
Mike Roylance <roylance.michael@gmail.com>
2014-02-02 13:15:23 -0700

    this looks like it will work well

--- hw3/source/tests.py
+++ hw3/source/tests.py
@@ -20,6 +20,51 @@ class GrammarInduce(unittest.TestCase):
 		self.assertTrue(pos[0] == ('mike', 'NP'))
 		self.assertTrue(pos[1] == ('walks', 'VP'))
 
+	def test_exampleGrammarWiki(self):
+		# arrange
+		sent = """
+(S 
+	(NP she) 
+	(VP 
+		(VP 
+			(V eats) 
+			(NP 
+				(Det a) 
+				(N fish))) 
+		(PP 
+			(P with) 
+			(NP (Det a) 
+				(N fork)))))
+		"""
+
+		# act
+		tree = nltk.Tree.parse(sent)
+
+		# assert
+		productions = tree.productions()
+		
+		first = productions[0]
+		self.assertTrue(str(first.lhs()) == "S")
+		self.assertTrue(str(first.rhs()[0]) == "NP")
+		self.assertTrue(str(first.rhs()[1]) == "VP")
+
+		second = productions[1]
+		self.assertTrue(str(second.lhs()) == "NP")
+		self.assertTrue(str(second.rhs()[0]) == "she")
+		
+		third = productions[2]
+		self.assertTrue(str(third.lhs()) == "VP")
+		self.assertTrue(str(third.rhs()[0]) == "VP")
+		self.assertTrue(str(third.rhs()[1]) == "PP")
+
+		fourth = productions[3]
+		self.assertTrue(str(fourth.lhs()) == "VP")
+		self.assertTrue(str(fourth.rhs()[0]) == "V")
+		self.assertTrue(str(fourth.rhs()[1]) == "NP")
+
+		# I think we get the picture...
+
+
 def main():
     unittest.main()
 
