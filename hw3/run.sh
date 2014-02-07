45669536d8f344a4b6317ec619a29fd3e611299c
Mike Roylance <roylance.michael@gmail.com>
2014-02-06 20:18:27 -0700

    changing run.sh

--- hw3/run.sh
+++ hw3/run.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
-(python2.7 ./source/main.py $1 $2;
+(python2.7 ./source_Olga/source/main.py $1 $2;
 
 /dropbox/13-14/571/hw3/tools/evalb -p /dropbox/13-14/571/hw3/tools/COLLINS.prm /dropbox/13-14/571/hw3/data/parses.test parses.hyp > parses.hyp.eval)
