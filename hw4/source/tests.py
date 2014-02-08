f47de163c02fef67311a59c5d143400028389d88
Mike Roylance <roylance.michael@gmail.com>
2014-02-08 16:20:20 -0700

    up to six sentences

--- hw4/source/tests.py
+++ hw4/source/tests.py
@@ -125,6 +125,59 @@ class ParseResult(unittest.TestCase):
 		# assert
 		self.assertTrue(len(bestParse) == 0)
 
+	def test_fourthSentence(self):
+		# arrange
+		builder = parseResult.ParseResult("../docs/grammar.fcfg")
+		sentence = "the dogs barks ." 
+
+		# act
+		bestParse = builder.build(sentence)
+
+		# assert
+		self.assertTrue(len(bestParse) == 0)
+
+	def test_fifthSentence(self):
+		# arrange
+		builder = parseResult.ParseResult("../docs/grammar.fcfg")
+		sentence = "the dog bark ." 
+
+		# act
+		bestParse = builder.build(sentence)
+
+		# assert
+		self.assertTrue(len(bestParse) == 0)
+
+	def test_sixthSentence(self):
+		# arrange
+		builder = parseResult.ParseResult("../docs/grammar.fcfg")
+		sentence = "dog barks ." 
+
+		# act
+		bestParse = builder.build(sentence)
+
+		# assert
+		self.assertTrue(len(bestParse) == 1)
+		
+		firstTree = bestParse[0]
+		self.assertTrue(queryFeatureProduction.getPos(firstTree.node) == "S")
+
+		secondTree = firstTree[0]
+		self.assertTrue(queryFeatureProduction.getPos(secondTree.node) == "NP")
+
+		fourthTree = secondTree[0]
+		self.assertTrue(queryFeatureProduction.getPos(fourthTree.node) == "N")
+		self.assertTrue(queryFeatureProduction.getTerminal(fourthTree) == "dog")
+		self.assertTrue(queryFeatureProduction.getNum(fourthTree.node) == "sg")
+
+		fifthTree = firstTree[1]
+		self.assertTrue(queryFeatureProduction.getPos(fifthTree.node) == "VP")
+		self.assertTrue(queryFeatureProduction.getTerminal(fifthTree) == "barks")
+		self.assertTrue(queryFeatureProduction.getNum(fifthTree.node) == "sg")
+
+		sixthTree = firstTree[2]
+		self.assertTrue(queryFeatureProduction.getPos(sixthTree.node) == "PUNC")
+		self.assertTrue(queryFeatureProduction.getTerminal(sixthTree) == ".")
+
 def main():
     unittest.main()
 
