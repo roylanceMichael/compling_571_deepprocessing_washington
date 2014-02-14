a1b17a3c28418811eaa6fdd5eb4a7cf8dc6a5411
Mike Roylance <roylance.michael@gmail.com>
2014-02-13 21:44:24 -0700

    changed the results file name

--- hw4/hw4.cmd
+++ hw4/hw4.cmd
@@ -8,7 +8,7 @@
 Universe   = vanilla
  
 Executable  = hw4.sh
-Arguments   = "docs/grammar.fcfg docs/feature_sentences.txt result"
+Arguments   = "docs/grammar.fcfg docs/feature_sentences.txt results"
 Log         = hw4.log
 Output      = hw4.out
 Error       = hw4.err
