656112b666efeb3092ce40f2a4ca39ca0b176409
Mike Roylance <roylance.michael@gmail.com>
2014-02-16 12:46:42 -0700

    first test around proposition logic

--- hw5/source/tests.py
+++ hw5/source/tests.py
@@ -2,6 +2,21 @@
 import unittest
 import nltk
 
+class PropositionLogic(unittest.TestCase):
+	def test_verifyBeingNorthIsAnAssymetricalStatement(self):
+		# arrange
+		lp = nltk.LogicParser()
+		SnF = lp.parse('SnF')
+		NotFns = lp.parse('-FnS')
+		R = lp.parse('SnF -> -FnS')
+		prover = nltk.Prover9()
+
+		# act
+		result = prover.prove(NotFns, [SnF, R])
+
+		# assert
+		self.assertTrue(result)
+
 def main():
     unittest.main()
 
