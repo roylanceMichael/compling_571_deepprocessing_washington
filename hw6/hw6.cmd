671554ac4648f629550652b89efcdb18e70eff64
roylanceMichael <roylance.michael@gmail.com>
2014-03-02 09:12:16 -0700

    updated structure

---
+++ hw6/hw6.cmd
@@ -0,0 +1,16 @@
+####################
+#
+# Mike Roylance UW NetID roylance
+# LING 571 Homework 6
+#
+####################
+ 
+Universe   = vanilla
+ 
+Executable  = hw6.sh
+Arguments   = ""
+Log         = hw6.log
+Output      = hw6.out
+Error       = hw6.err
+Notification=Error
+Queue
