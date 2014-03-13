fe11d7b1d08e4f4bd9d99fc9019e2e914981a62c
Mike Roylance <roylance.michael@gmail.com>
2014-03-12 23:06:22 -0600

    a few more notes

--- hw7/source/hobbs.py
+++ hw7/source/hobbs.py
@@ -15,3 +15,11 @@ class Hobbs:
 	def process(self):
 		print self.firstTree
 		print self.secondTree
+
+		# TODO
+		# start with target pronoun (in secondtree)
+		# climb parse tree to S root (of firsttree)
+		# for each NP or S
+		# breadth first search, left to right, of children
+
+		# so ya, I've done this before... let's finish tomorrow...
