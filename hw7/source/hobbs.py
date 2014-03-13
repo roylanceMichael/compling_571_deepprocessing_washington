ac4e867cbaff8f62a99076064b0910cea5c891f1
Mike Roylance <roylance.michael@gmail.com>
2014-03-12 22:33:37 -0600

    enough for tonight...

--- hw7/source/hobbs.py
+++ hw7/source/hobbs.py
@@ -7,4 +7,11 @@ class Hobbs:
 		self.firstTree = firstTree
 		self.secondTree = secondTree
 
-	
+		# hard coding the pronouns, as found in ../docs/grammar.cfg
+		self.pronouns = {}
+		self.pronouns["PRP"] = None
+		self.pronouns["PossPro"] = None
+
+	def process(self):
+		print self.firstTree
+		print self.secondTree
--- hw7/source/main.py
+++ hw7/source/main.py
@@ -3,20 +3,26 @@ import sys
 import nltk
 import hobbs
 
+# method for handling the second sentence found in a pair
 def handleWhenSecondSentence(firstSentence, secondSentence, parser):
+	# tokenize the sentences
 	firstTokenizedSentence = nltk.word_tokenize(firstSentence)
 	secondTokenizedSentence = nltk.word_tokenize(secondSentence)
 
+	# parse them
 	firstTrees = parser.nbest_parse(firstTokenizedSentence)
 	secondTrees = parser.nbest_parse(secondTokenizedSentence)
 
+	# verify correct parses
 	if len(firstTrees) == 0 or len(secondTrees) == 0:
 		print 'invalid parse!'
 		print firstTokenizedSentence
 		print secondTokenizedSentence
 		return
 
-	hobbsInts = hobbs.Hobbs(firstTrees[0], secondTrees[0])
+	# instantiate hobbs logic
+	hobbsInst = hobbs.Hobbs(firstTrees[0], secondTrees[0])
+	hobbsInst.process()
 
 def main():
 	# arg variables
@@ -33,13 +39,17 @@ def main():
 	sentence = sentenceFileStream.readline()
 
 	while sentence:
+		# first, strip sentence so we can detect empty string
 		strippedSentence = sentence.strip()
+		# add if length is 0
 		if len(sentenceList) == 0 and len(strippedSentence) > 0:
 			sentenceList.append(strippedSentence)
+		# process if length is 1
 		elif len(sentenceList) == 1 and len(strippedSentence) > 0:
 			handleWhenSecondSentence(sentenceList[0], strippedSentence, parser)
 			sentenceList = []
 
+		# read new sentence
 		sentence = sentenceFileStream.readline()
 
 if __name__ == '__main__':
