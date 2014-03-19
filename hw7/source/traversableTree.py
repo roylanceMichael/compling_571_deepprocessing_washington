1cab6a1cc7512f23b5e99e2fc1f175c7064df8da
Mike Roylance <roylance.michael@gmail.com>
2014-03-18 23:27:19 -0600

    updated to not include the pronoun

--- hw7/source/traversableTree.py
+++ hw7/source/traversableTree.py
@@ -37,13 +37,13 @@ class TraversableTree:
 	def setIsPronoun(self, rules):
 		if self.pos in rules.acceptablePronouns:
 			self.isPronoun = True
-		#	self.hasPronouns = True
+			self.hasPronouns = True
 			return
 
-		# for item in self.items:
-		#	if str(item.lhs()) in rules.acceptablePronouns:
-		#		self.hasPronouns = True
-		#		return
+		for item in self.items:
+			if str(item.lhs()) in rules.acceptablePronouns:
+				self.hasPronouns = True
+				return
 
 		self.isPronoun = False
 
